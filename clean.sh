4c410d24bf0524de535f47800fd53eb929bc9688
Flame Sage <chris062689@gmail.com>
2023-05-04 19:37:31 -0400

    Updated clean.sh script.

--- clean.sh
+++ clean.sh
@@ -9,18 +9,22 @@ git pull
 
 cd /git/server/app/
 rm -rf node_modules || true
+npm install
 
 cd /git/server/src-discord/
 rm app.log || true
 rm app-error.log || true
 rm -rf node_modules || true
+npm install
 
 cd /git/server/src-website/
 rm app.log || true
 rm app-error.log || true
 rm -rf node_modules || true
+npm install
 
 cd /git/server/src-cron/
 rm app.log || true
 rm app-error.log || true
 rm -rf node_modules || true
+npm install
